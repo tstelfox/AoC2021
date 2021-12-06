412ac8ccc5ca2b085e4e67905cac8fc5b130d543
Turlough Mullan <duke1199sp@gmail.com>
2021-12-06 18:07:22 +0100

    Ok first exercise solved although I went through much pain to do it

--- day4/squid.py
+++ day4/squid.py
@@ -2,32 +2,57 @@
 # import os
 from array import *
 
+def	calculate_points(grid, winner):
+	points = 0
+	for row in grid:
+		for i in row:
+			if i != 'x':
+				points += int(i)
+	print(int(winner) * points)
+	return True
+
+def	check_winner(grids, winner):
+	g = 0
+	bingo = 0
+	for grid in grids:
+		h = 0
+		i = 0
+		while i in range(len(grid)):
+			while h in range(len(grid)):
+				if grid[h][i] == 'x':
+					bingo += 1
+				if bingo == 5:
+					return calculate_points(grid, winner)
+				h += 1
+			i += 1
+			h = 0
+			bingo = 0
+		i = 0
+		while h in range(len(grid)):
+			while i in range(len(grid)):
+				if grid[h][i] == 'x':
+					bingo += 1
+				if bingo == 5:
+					return calculate_points(grid, winner)
+				i += 1
+			h += 1
+			i = 0
+			bingo = 0
+		bingo = 0
+		g += 1
 
 def	solve_grids(grids, answers):
-	
-	# for i in range(len(answers)):
-	# 	num = answers[i]
-	# 	g = 0
-	# 	for grid in grids:
-
-	
-	
 	for num in answers:
 		g = 0
 		for grid in grids:
 			for i in range(len(grid)):
-				if i == 0:
-					i += 1
-				for k in range(6):
-					print(grids[g][k])
-					# Getting there?
-					# if num == grid[g][i][k]:
-						# print("Pino sei grande")
-						# print(grid[i][k])
-						# print(i, k)
-						# grids[g][i][k] = 'x'
+				for k in range(5):
+					if num == grids[g][k][i]:
+						winner = num
+						grids[g][k][i] = 'x'
 			g += 1
-	# print(grids)
+		if check_winner(grids, winner) is True:
+			return
 		
 		
 
@@ -38,45 +63,23 @@ answers = list(map(str.strip, answers))
 
 grid_data = fp.readlines()
 
+# The following three lines caused me grief because I was wrapping my head around how lists work in python
 grids = [grid_data[i: i + 6] for i in range(0, len(grid_data), 6)]
 grids = [list(map(str.strip, grid)) for grid in grids]
 
 grids = [line.split() for grid in grids for line in grid]
-	# for line in gr()id:
-	# 	line.split
 
+grid = []
+realgrids = []
+i = 0
+for row in grids:
+	if i != 0:
+		grid.append(row)
+	i += 1
+	if i == 6:
+		i = 0
+		realgrids.append(grid)
+		grid = []
 
+solve_grids(realgrids, answers)
 
-solve_grids(grids, answers)
-
-# print(grids)
-
-# for line in grid_data:
-# 	if i == 6:
-# 		grids.append(grid)
-# 		del(grid)
-# 		i = 0
-# 	grid.append(line.split())
-# 	i += 1
-
-print(grids[0])
-
-# print(grids)
-
-
-# grids = []
-# grid = []
-# ln_cnt = 0
-# for line in grid_data:
-# 	if line == 6:
-# 		grid.append(line)
-# 		ln_cnt += 1
-# 		if ln_cnt == 5:
-# 			grids.append(grid)
-# 			ln_cnt = 0
-# 			del(grid)
-
-
-# print(grids)
-
-# print(answers)
