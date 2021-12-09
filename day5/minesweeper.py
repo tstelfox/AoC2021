7e1c2bbf50f08219073ad52a8dc716e83fd3539c
Turlough stelfox Mullan <duke1199sp@gmail.com>
2021-12-09 13:05:18 +0100

    Thankyou Peer for the wisdom

--- day5/minesweeper.py
+++ day5/minesweeper.py
@@ -30,26 +30,37 @@ def	trace_line(grid, fromx, fromy, tox, toy):
 	fromy = int(fromy)
 	tox = int(tox)
 	toy = int(toy)
-	finalx = int(max(fromx, tox))
-	startx = int(min(fromx, tox))
-	finaly = int(max(fromy, toy))
-	starty = int(min(fromy, toy))
-
 	
-	while startx <= finalx and starty <= finaly:
-		grid[starty][startx] = 1 if grid[starty][startx] == 0 else grid[starty][startx] + 1
-		if starty == finaly and startx == finalx:
-			break
-		if startx < finalx:
-			startx += 1
-		if starty < finaly:
-			starty += 1
+
+	if fromx == tox:
+		finaly = int(max(fromy, toy))
+		starty = int(min(fromy, toy))
+		while starty <= finaly:
+			grid[starty][int(fromx)] += 1
+			if starty <= finaly:
+				starty += 1
+		return grid
+	elif fromy == toy:
+		finalx = int(max(fromx, tox))
+		startx = int(min(fromx, tox))
+		while startx <= finalx:
+			grid[int(fromy)][startx] += 1
+			if startx <= finalx:
+				startx += 1
+		return grid
+	elif fromx != tox and fromy != toy:
+		while True:
+			grid[fromy][fromx] += 1
+			if fromx == tox and fromy == toy:
+				break
+			fromx += 1 if tox > fromx else -1
+			fromy += 1 if toy > fromy else -1
 	return grid
 
 
 fp = open('day5/inputfile', mode = 'r').readlines()
 
-grid = [[int(0) for i in range(10)] for j in range(10)]
+grid = [[int(0) for i in range(1000)] for j in range(1000)]
 
 for line in fp:
 	info = line.strip().split()
@@ -58,6 +69,4 @@ for line in fp:
 	grid = trace_line(grid, da[0], da[1], a[0], a[1])
 
 
-for line in grid:
-	print(line)
 print(find_cross(grid))
