b4ac6dd5a1efa491a6a0e45b1d290400fb1f8c9a
Turlough stelfox Mullan <duke1199sp@gmail.com>
2021-12-09 19:18:33 +0100

    Damnit I wanna really learn how to do those made for loops

--- day8/idunno.py
+++ day8/idunno.py
@@ -1,2 +1,17 @@
 lines = open('inputfile').read().splitlines()
 
+cnt_easy = 0
+lines = [line.split('|') for line in lines]
+
+clocks = []
+
+for line in lines:
+    clocks.append(line[1])
+
+for clock in clocks:
+    count_now = clock.split()
+    for items in count_now:
+        if len(items) >= 2 and len(items) <= 4 or len(items) == 7:
+            cnt_easy += 1
+
+print(cnt_easy)
