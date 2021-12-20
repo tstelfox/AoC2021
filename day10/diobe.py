898ce04643f0d69e35ca5814637cb883928cd36e
Turlough Mullan <duke1199sp@gmail.com>
2021-12-20 14:47:14 +0100

    Working on day 10

--- day10/diobe.py
+++ day10/diobe.py
@@ -1,4 +1,6 @@
 
+import statistics
+
 def verify_closure(bracket, lastopened):
     if bracket == ')':
         if lastopened[len(lastopened) - 1] != 0:
@@ -19,7 +21,7 @@ def verify_closure(bracket, lastopened):
         lastopened.pop()
     return 0
 
-def check_line(input):
+def check_corrupt(input):
     # types = [0] * 4
     # print(types)
     lastopened = []
@@ -50,14 +52,68 @@ def check_line(input):
             return ret
     return 0
 
+def check_rest(input):
+    types = [0] * 4
+    lastopened = []
+    score = 0
+
+    for bracket in input:
+        if bracket == '(':
+            lastopened.append(0)
+            types[0] += 1
+        if bracket == '[':
+            lastopened.append(1)
+            types[1] += 1
+        if bracket == '{':
+            lastopened.append(2)
+            types[2] += 1
+        if bracket == '<':
+            lastopened.append(3)
+            types[3] += 1
+        if bracket == ')':
+            types[0] -= 1
+            lastopened.pop()
+        if bracket == ']':
+            types[1] -= 1
+            lastopened.pop()
+        if bracket == '}':
+            types[2] -= 1
+            lastopened.pop()
+        if bracket == '>':
+            types[3] -= 1
+            lastopened.pop()
+    for bracket in lastopened:
+        print(score)
+        score *= 5
+        if lastopened[len(lastopened) - 1] == 0:
+            score += 1
+        elif lastopened[len(lastopened) - 1] == 1:
+            score += 2
+        elif lastopened[len(lastopened) - 1] == 2:
+            score += 3
+        elif lastopened[len(lastopened) - 1] == 3:
+            score += 4
+        lastopened.pop()
+        # if ret := verify_closure(bracket, lastopened):
+        #     return ret
+    return score
+
 
 if __name__ == '__main__':
-    lines = open("inputfile.txt").read().splitlines()
+    lines = open("testfile.txt").read().splitlines()
 
+incomplete = []
 point = 0
 for line in lines:
     bracket = 0
-    if bracket := check_line(line):
-        # print(bracket)
+    if bracket := check_corrupt(line):
         point += bracket
-print(point)
+    else:
+        incomplete.append(line)
+# print(point)
+# print(incomplete)
+points2 = []
+for line in incomplete:
+    points2.append(check_rest(line))
+# print(points2)
+print(statistics.median(points2))
