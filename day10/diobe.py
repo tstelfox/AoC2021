6e6c5e5d39e04d8c2c98b8a610417dc932d67855
Codemuncher <duke1199sp@gmail.com>
2021-12-20 15:03:20 +0100

    Ok cleaned it up but now I wanna learn how to write this in a cleaner wayt

--- day10/diobe.py
+++ day10/diobe.py
@@ -12,7 +12,6 @@ def verify_closure(bracket, lastopened):
         lastopened.pop()
     if bracket == '}':
         if lastopened[len(lastopened) - 1] != 2:
-            # print(lastopened)
             return 1197
         lastopened.pop()
     if bracket == '>':
@@ -22,85 +21,59 @@ def verify_closure(bracket, lastopened):
     return 0
 
 def check_corrupt(input):
-    # types = [0] * 4
-    # print(types)
     lastopened = []
 
     for bracket in input:
-        # print(lastopened)
         if bracket == '(':
             lastopened.append(0)
-            # types[0] += 1
         if bracket == '[':
             lastopened.append(1)
-            # types[1] += 1
         if bracket == '{':
             lastopened.append(2)
-            # types[2] += 1
         if bracket == '<':
             lastopened.append(3)
-            # types[3] += 1
-        # if bracket == ')':
-        #     types[0] -= 1
-        # if bracket == ']':
-        #     types[1] -= 1
-        # if bracket == '}':
-        #     types[2] -= 1
-        # if bracket == '>':
-        #     types[3] -= 1
         if ret := verify_closure(bracket, lastopened):
             return ret
     return 0
 
 def check_rest(input):
-    types = [0] * 4
     lastopened = []
     score = 0
-
+    # Keep track of the brackets opened and then closed leaving those unclosed
     for bracket in input:
         if bracket == '(':
             lastopened.append(0)
-            types[0] += 1
         if bracket == '[':
             lastopened.append(1)
-            types[1] += 1
         if bracket == '{':
             lastopened.append(2)
-            types[2] += 1
         if bracket == '<':
             lastopened.append(3)
-            types[3] += 1
         if bracket == ')':
-            types[0] -= 1
             lastopened.pop()
         if bracket == ']':
-            types[1] -= 1
             lastopened.pop()
         if bracket == '}':
-            types[2] -= 1
             lastopened.pop()
         if bracket == '>':
-            types[3] -= 1
             lastopened.pop()
-    for bracket in lastopened:
-        print(score)
+    # Check the last bracket opened and score according to closing bracket
+    for i in range(len(lastopened)):
         score *= 5
-        if lastopened[len(lastopened) - 1] == 0:
+        if lastopened[len(lastopened) -1] == 0:
             score += 1
-        elif lastopened[len(lastopened) - 1] == 1:
+        elif lastopened[len(lastopened) -1] == 1:
             score += 2
-        elif lastopened[len(lastopened) - 1] == 2:
+        elif lastopened[len(lastopened) -1] == 2:
             score += 3
-        elif lastopened[len(lastopened) - 1] == 3:
+        elif lastopened[len(lastopened) -1] == 3:
             score += 4
         lastopened.pop()
-        # if ret := verify_closure(bracket, lastopened):
-        #     return ret
     return score
 
 
 if __name__ == '__main__':
-    lines = open("testfile.txt").read().splitlines()
+    lines = open("inputfile.txt").read().splitlines()
 
 incomplete = []
 point = 0
@@ -110,10 +83,8 @@ for line in lines:
         point += bracket
     else:
         incomplete.append(line)
-# print(point)
-# print(incomplete)
 points2 = []
 for line in incomplete:
     points2.append(check_rest(line))
-# print(points2)
+print(points2)
 print(statistics.median(points2))
